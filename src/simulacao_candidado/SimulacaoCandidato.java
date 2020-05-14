96fddf74dc9a6cbde53120427234589ff54d9ace
Marcos Vinício <marcosviniciohd@hotmail.com>
2020-05-13 22:52:45 -0300

    Leitura da escolaridade

--- src/simulacao_candidado/SimulacaoCandidato.java
+++ src/simulacao_candidado/SimulacaoCandidato.java
@@ -1,10 +1,44 @@
 package simulacao_candidado;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class SimulacaoCandidato {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//Variáveis escolaridade
+		int ensinoFundamental = 10;
+		int ensinoMedio = 20;
+		int ensinoSuperior = 30;
+		int posGraduacao = 40;
+		
+		//Variáveis Experiência
+		int semExperiencia = 0;
+		int doisAnosExperiencia = 10;
+		int entreDoisECincoAnosExperiencia = 20;
+		int maisDeCincoAnosExperiencia = 40;
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médiol");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		
+		System.out.print("Digite uma opção: ");
+		int escolaridade = sc.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
